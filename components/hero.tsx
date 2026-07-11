0c514bdc90fedc162484e1c1a2e31f3f423ea086
David Gutierrez <cellzt001@gmail.com>
2026-07-10 22:48:15 -0500

    fix: size files optimized (video)

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,9 +1,9 @@
 export default function Hero() {
     return (
-        <div className="relative h-svh w-screen overflow-hidden">
+        <div className="relative h-svh w-screen overflow-hidden flex flex-col items-center justify-center">
             <video
-                src="/videos/rally.mp4"
-                className="h-full w-full object-cover"
+                src="/videos/hero.mp4"
+                className="h-full w-full object-cover opacity-70"
                 autoPlay
                 muted
                 loop
@@ -15,18 +15,26 @@ export default function Hero() {
                 style={{
                     backgroundImage:
                         'url("https://framerusercontent.com/images/6mcf62RlDfRfU61Yg5vb2pefpi4.png")',
-                    backgroundSize: "200px",
+                    backgroundSize: "220px",
                 }}
             />
 
-            <div className="absolute top-1/2 -translate-y-1/2 left-[10%] font-bebas font-bold leading-none border-b-3 pe-10">
-                <h1 className="text-[clamp(4rem,6vw,8rem)]">DONDE EL <br />
-                    <span className="text-[clamp(5rem,7vw,9rem)]">
-                        DEPORTE
-                    </span><br />
-                    COBRA VIDA.
-                </h1>
+            <div className="absolute top-1/2 -translate-y-1/2 flex max-w-450 w-full justify-between px-[clamp(1rem,5vw,5rem)] flex-col md:flex-row gap-10">
+                <div className="font-bebas font-bold leading-none border-b-3 pe-10">
+                    <h1 className="text-[clamp(4rem,6vw,8rem)]">DONDE EL <br />
+                        <span className="text-[clamp(5rem,7vw,9rem)]">
+                            DEPORTE
+                        </span><br />
+                        COBRA VIDA.
+                    </h1>
+                </div>
+
+                <div className="font-light text-xl text-white/80 max-w-100">
+                    <p>Descubre, explora y participa en eventos deportivos en tu zona. Encuentra actividades de diferentes disciplinas, vive experiencias únicas en tu ciudad.</p>
+                </div>
             </div>
+
+
         </div>
     )
 }
