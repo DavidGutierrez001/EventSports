adceb375d65f131753f46cf62313cd0ff3860179
David Gutierrez <cellzt001@gmail.com>
2026-07-12 15:16:25 -0500

    feat: new button register

--- components/hero.tsx
+++ components/hero.tsx
@@ -19,18 +19,21 @@ export default function Hero() {
                 }}
             />
 
-            <div className="absolute top-1/2 -translate-y-1/2 flex max-w-450 w-full justify-between px-[clamp(1rem,5vw,5rem)] flex-col md:flex-row gap-10">
-                <div className="font-bebas font-bold leading-none border-b-3 pe-10">
-                    <h1 className="text-[clamp(4rem,6vw,8rem)]">DONDE EL <br />
-                        <span className="text-[clamp(5rem,7vw,9rem)]">
-                            DEPORTE
-                        </span><br />
+            <div className="absolute top-1/2 -translate-y-1/2 flex max-w-460 w-full justify-between px-[clamp(1rem,5vw,5rem)] flex-col md:flex-row gap-10">
+                <div className="font-bebas font-bold leading-none border-b-3 max-w-90 min-w-90 md:min-w-fit md:max-w-fit pe-10">
+                    <h1 className="text-[clamp(4rem,6vw,8rem)]">
+                        DONDE EL <br />
+                        <span className="text-[clamp(5rem,7vw,9rem)]">DEPORTE</span><br />
                         COBRA VIDA.
                     </h1>
                 </div>
 
-                <div className="font-light text-xl text-white/80 max-w-100">
-                    <p>Descubre, explora y participa en eventos deportivos en tu zona. Encuentra actividades de diferentes disciplinas, vive experiencias únicas en tu ciudad.</p>
+                <div className="font-light text-xl md:text-[2xl] text-white max-w-100 flex flex-col justify-center items-start overflow-hidden">
+                    <p>Descubre, explora, gestiona y participa en eventos deportivos en tu zona. Encuentra actividades de diferentes disciplinas y divisiones.
+                    </p>
+                    <button className="flex w-full items-center btn-selected cursor-pointer p-4 md:p-5 text-lg text-black font-medium justify-center backdrop-blur-sm bg-white/90 hover:bg-white rounded shadow-md mt-10 transition-colors">
+                        COMIENZA AHORA
+                    </button>
                 </div>
             </div>
 
